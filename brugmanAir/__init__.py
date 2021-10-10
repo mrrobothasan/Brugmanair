9b56690228870efd824217da56ed4cba450076cf
Majd <majd.135.mh@gmail.com>
2021-10-10 22:56:38 +0200

    updated

--- brugmanAir/__init__.py
+++ brugmanAir/__init__.py
@@ -11,16 +11,13 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "06840564M#2"
 app.config["MYSQL_DB"] = "brugmanair"
-app.secret_key = "secret123"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = "secret123"
 
 # init MYSQL
 mysql = MySQL(app)
-
-
 
 from brugmanAir.flight import routes
 from brugmanAir.beheerder import routes
